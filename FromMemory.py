a607b364e4d6383f83fc73c20c92192a0b064203
Ian McBratney <ian.mcbratney@gmail.com>
2017-12-30 22:26:32 -0900

    Day 2

--- FromMemory.py
+++ FromMemory.py
@@ -5,11 +5,8 @@ import numpy as np
 ##Variables
 np.random.seed(1)
 InputArray = [[0,0,1],[0,1,1],[1,0,1],[1,1,1]]
-
 OutputArray = [[0,0,1,1]]
-
 CycleCount = 10000
-np.random.seed(1)
 
 ##Setup
 #sigmoid function
@@ -22,14 +19,26 @@ def nonlin(x, deriv=False):
 X = np.array(InputArray)
 
 # output dataset
-Y = np.array(OutputArray.T)
+Y = np.array(OutputArray).T
 
 ##Logic
 #initialize weights randomly with mean 0
 syn0 = 2*np.random.random((3,1)) - 1
 
-for iter in xrange(CycleCount):
+for iter in range(CycleCount):
 
     # forward propagation
     l0 = X
     l1 = nonlin(np.dot(l0,syn0))
+
+    # how much did we miss?
+    l1_error = Y - l1
+    # multiply how much we missed by the
+    # slope of the sigmoin at the value in l1
+    l1_delta = l1_error * nonlin(l1,True)
+
+    # update weights
+    syn0 += np.dot(l0.T,l1_delta)
+
+print("Output After Training")
+print(l1)
