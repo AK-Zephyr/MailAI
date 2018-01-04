0d06416b926cf407677e15ebdf2423f7fd99b11d
Ian McBratney <ian.mcbratney@gmail.com>
2018-01-03 23:24:12 -0900

    Variables Updated

--- FromMemory.py
+++ FromMemory.py
@@ -1,37 +1,43 @@
 #This Script is to test my ability to recall the basic functions of a NN from memory.
-
+###########
+##imports
+###########
 import numpy as np
-
+###########
 ##Variables
+###########
 np.random.seed(1)
-InputArray = [[0,0,1],[0,1,1],[1,0,1],[1,1,1]]
-OutputArray = [[0,0,1,1]]
-CycleCount = 10000
-
-##Setup
-#sigmoid function
+input_array = [[0,0,1],[0,1,1],[1,0,1],[1,1,1]]
+output_array = [[0,0,1,1]]
+cycle_count = 10000
+###########
+##Functions
+###########
 def nonlin(x, deriv=False):
-    """"""
-
-        if(deriv==True):
-            return x*(1-x)
-        return 1/(1+np.exp(-x))
-
+    """This is a sigmoid function
+    (input,True=return derivative)"""
+    if(deriv==True):
+        return x*(1-x)
+    return 1/(1+np.exp(-x))
+###########
+##Variable Setup
+###########
 # input dataset
-Import = np.array(InputArray)
-
+data = np.array(InputArray)
 # output dataset
-Goal = np.array(OutputArray).T
-
+goal = np.array(OutputArray).T
+###########
 ##Logic
+###########
 #initialize weights randomly with mean 0
-Seed = 2*np.random.random((3,1)) - 1
+array_seed = 2*np.random.random((3,1)) - 1
 
 for iter in range(CycleCount):
 
+
     # forward propagation
-    l0 = Import
-    l1 = nonlin(np.dot(l0,Seed))
+    l0 = data
+    l1 = nonlin(np.dot(l0,array_seed))
 
     # how much did we miss?
     l1_error = Y - l1
@@ -40,7 +46,7 @@ for iter in range(CycleCount):
     l1_delta = l1_error * nonlin(l1,True)
 
     # update weights
-    syn0 += np.dot(l0.T,l1_delta)
+    array_seed += np.dot(l0.T,l1_delta)
 
 print("Output After Training")
 print(l1)
