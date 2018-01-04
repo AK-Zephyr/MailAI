b9ca8766d5df1950e283f691d59aa89f593eef3c
Ian McBratney <ian.mcbratney@gmail.com>
2018-01-04 00:18:29 -0900

    Logging
    Setup per runtime logging

--- FromMemory.py
+++ FromMemory.py
@@ -1,8 +1,12 @@
 #This Script is to test my ability to recall the basic functions of a NN from memory.
 ###########
-##imports
+##Imports
 ###########
 import numpy as np
+###########
+##Outputs
+###########
+log = open("log.txt","w")
 ###########
 ##Variables
 ###########
@@ -10,6 +14,8 @@ np.random.seed(1)
 input_array = [[0,0,1],[0,1,1],[1,0,1],[1,1,1]]
 output_array = [[0,0,1,1]]
 cycle_count = 10000
+#log per how many cycles
+log_amount = 100
 ###########
 ##Functions
 ###########
@@ -31,22 +37,26 @@ goal = np.array(OutputArray).T
 ###########
 #initialize weights randomly with mean 0
 array_seed = 2*np.random.random((3,1)) - 1
-
-for iter in range(CycleCount):
-
-
+print("Array Seed")
+print (array_seed)
+for iter in range(cycle_count):
     # forward propagation
+    knowledge = array_seed
     l0 = data
     l1 = nonlin(np.dot(l0,array_seed))
-
     # how much did we miss?
     l1_error = Y - l1
     # multiply how much we missed by the
     # slope of the sigmoin at the value in l1
     l1_delta = l1_error * nonlin(l1,True)
-
     # update weights
-    array_seed += np.dot(l0.T,l1_delta)
+    knowledge += np.dot(l0.T,l1_delta)
+
+    # Logging
+    if not (iter % log_amount):
+        log.writelines(str(iter) + "\n")
+        log.writelines(str(l1_error) + "\n")
+
 
 print("Output After Training")
 print(l1)
